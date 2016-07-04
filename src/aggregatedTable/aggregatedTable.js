f59a006faeae73a4f8c227e12e642e1a12a594b3
jahglow <jahglow@gmail.com>
2016-07-04 18:38:33 +0300

    added FixedHeader class for aggregated Table to set a fixed header on tables with .reportal-fixed-header

--- src/aggregatedTable/aggregatedTable.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by IvanP on 04.07.2016.
- */
-class AggregatedTable {
-
-}
-export default AggregatedTable;
--- src/main.js
+++
@@ -1,7 +0,0 @@
-//const Cf = require('./lib/base.js');
-
-import Confirmit from './lib/base.js';
-
-import poll from './commonFunctions.js';
-
-import FixedHeader from './aggregatedTable/FixedHeader.js';
