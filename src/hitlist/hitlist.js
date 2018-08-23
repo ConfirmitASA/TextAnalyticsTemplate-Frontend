58bdf8236e25925c305097b1899c47a58047c93d
Anna Kozlova <fyz95@mail.ru>
2018-08-23 16:08:04 +0300

    Hitlist - info icon added

--- src/hitlist/hitlist.js
+++ src/hitlist/hitlist.js
@@ -1,24 +1,26 @@
 import FixedHeader from "../aggregatedTable/FixedHeader.js"
+
 var dateformat = require('dateformat');
 
 class Hitlist {
 
-  constructor({currentCategory = '', separator = ' ',hitlist, headers, hitlistData, dateTimeFormat, selectedWords, sentimentConfig =
-    [
-      {
-        sentiment: "positive",
-        icon:  `<div class="icon">
+  constructor({
+                currentCategory = '', separator = ' ', hitlist, headers, hitlistData, dateTimeFormat, selectedWords, sentimentConfig =
+      [
+        {
+          sentiment: "positive",
+          icon: `<div class="icon">
                   <svg class="cf_positive" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="15.5" cy="9.5" r="1.5"></circle>
                     <circle cx="8.5" cy="9.5" r="1.5"></circle>
                     <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm0-4c-1.48 0-2.75-.81-3.45-2H6.88c.8 2.05 2.79 3.5 5.12 3.5s4.32-1.45 5.12-3.5h-1.67c-.7 1.19-1.97 2-3.45 2z"></path>
                   </svg>
                 </div>`,
-        range: {min: 2, max: 5}
-      },
-      {
-        sentiment: "neutral",
-        icon:  `<div class="icon">
+          range: {min: 2, max: 5}
+        },
+        {
+          sentiment: "neutral",
+          icon: `<div class="icon">
                  <svg class="cf_neutral" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
                    <path d="M9 14h6v1.5H9z"></path>
                    <circle cx="15.5" cy="9.5" r="1.5"></circle>
@@ -26,29 +28,29 @@ class Hitlist {
                    <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
                  </svg>
                 </div>`,
-        range: {min: -1, max: 1}
-      },
+          range: {min: -1, max: 1}
+        },
 
-      {
-        sentiment: "negative",
-        icon:  `<div class="icon">
+        {
+          sentiment: "negative",
+          icon: `<div class="icon">
                   <svg class="cf_negative" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
                      <circle cx="15.5" cy="9.5" r="1.5"></circle>
                      <circle cx="8.5" cy="9.5" r="1.5"></circle>
                      <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm0-6c-2.33 0-4.32 1.45-5.12 3.5h1.67c.69-1.19 1.97-2 3.45-2s2.75.81 3.45 2h1.67c-.8-2.05-2.79-3.5-5.12-3.5z"></path>
                   </svg>
                 </div>`,
-        range: {min: -5, max: -2}
-      }
-    ], icons = {
-    "positive": `<div class="icon">
+          range: {min: -5, max: -2}
+        }
+      ], icons = {
+      "positive": `<div class="icon">
                   <svg class="cf_positive" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="15.5" cy="9.5" r="1.5"></circle>
                     <circle cx="8.5" cy="9.5" r="1.5"></circle>
                     <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm0-4c-1.48 0-2.75-.81-3.45-2H6.88c.8 2.05 2.79 3.5 5.12 3.5s4.32-1.45 5.12-3.5h-1.67c-.7 1.19-1.97 2-3.45 2z"></path>
                   </svg>
                 </div>`,
-    "neutral": `<div class="icon">
+      "neutral": `<div class="icon">
                  <svg class="cf_neutral" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
                    <path d="M9 14h6v1.5H9z"></path>
                    <circle cx="15.5" cy="9.5" r="1.5"></circle>
@@ -56,14 +58,15 @@ class Hitlist {
                    <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
                  </svg>
                 </div>`,
-    "negative": `<div class="icon">
+      "negative": `<div class="icon">
                   <svg class="cf_negative" height="48" viewBox="0 0 24 24" width="48" xmlns="http://www.w3.org/2000/svg">
                      <circle cx="15.5" cy="9.5" r="1.5"></circle>
                      <circle cx="8.5" cy="9.5" r="1.5"></circle>
                      <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm0-6c-2.33 0-4.32 1.45-5.12 3.5h1.67c.69-1.19 1.97-2 3.45-2s2.75.81 3.45 2h1.67c-.8-2.05-2.79-3.5-5.12-3.5z"></path>
                   </svg>
                 </div>`
-  }}={}) {
+    }
+              } = {}) {
     this.source = hitlist;
     this.headers = headers;
     this.hitlistData = hitlistData;
@@ -83,7 +86,9 @@ class Hitlist {
     this.processDates();
     this.processMainColumn();
     this.addIconsForSentiment();
-    if(!this.source.querySelector('.aggregatedTableContainer')){
+    this.addInfoText();
+
+    if (!this.source.querySelector('.aggregatedTableContainer')) {
       this.fixedHeader = new FixedHeader({source: this.source.querySelector('table')});
     } else { // hack to get pagination text and update an already initialised header since we'd need that new text on hitlist update
       this.source.querySelector('table.ta-fixed>thead').innerHTML = this.source.querySelector('table:not(.ta-fixed)>thead').innerHTML;
@@ -91,16 +96,17 @@ class Hitlist {
       /* BEGIN setting column header sizes after sorting */
       var initialHeader = this.source.querySelectorAll('table:not(.ta-fixed)>thead>tr>*');
       var clonedHeader = this.source.querySelectorAll('table.ta-fixed>thead>tr>*');
-      [].slice.call(clonedHeader).forEach((el,index) => {
-        el.style.width=initialHeader[index].offsetWidth+'px';
+      [].slice.call(clonedHeader).forEach((el, index) => {
+        el.style.width = initialHeader[index].offsetWidth + 'px';
       });
-      clonedHeader.style.width = initialHeader.offsetWidth+'px';
+      clonedHeader.style.width = initialHeader.offsetWidth + 'px';
       /* END */
 
       var offset = this.source.querySelector('table:not(.ta-fixed)').parentNode.offsetTop;
-      this.scrollTo(offset,200);
+      this.scrollTo(offset, 200);
     }
   }
+
   /**
    * Implements smooth srolling
    * @param {Number} to - offset from top of the page the window needs to be scrolled to
@@ -112,11 +118,11 @@ class Hitlist {
       currentTime = 0,
       increment = 20;
 
-    var animateScroll = function(){
+    var animateScroll = function () {
       currentTime += increment;
       var val = easeInOutQuad(currentTime, start, change, duration);
-      window.scrollTo(0,val);
-      if(currentTime < duration) {
+      window.scrollTo(0, val);
+      if (currentTime < duration) {
         setTimeout(animateScroll, increment);
       }
     };
@@ -126,16 +132,16 @@ class Hitlist {
     //b = start value
     //c = change in value
     //d = duration
-    function easeInOutQuad (t, b, c, d) {
-      t /= d/2;
-      if (t < 1) return c/2*t*t + b;
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
       t--;
-      return -c/2 * (t*(t-2) - 1) + b;
+      return -c / 2 * (t * (t - 2) - 1) + b;
     }
   }
 
 
-  addClassesToHitlist(question,type) {
+  addClassesToHitlist(question, type) {
     let questionCells = this.source.querySelectorAll(".yui3-datatable-col-" + question.name);
     [].slice.call(questionCells).forEach(item => {
         item.classList.add("reportal-hitlist-" + type.postfix);
@@ -145,13 +151,13 @@ class Hitlist {
     )
   }
 
-  changeTitles(question){
-    if(question.title){
+  changeTitles(question) {
+    if (question.title) {
       this.source.querySelector(".yui3-datatable-header.yui3-datatable-col-" + question.name).innerHTML = question.title;
     }
   }
 
-  processHeadersConfig(){
+  processHeadersConfig() {
     const types = [
       {type: "verbatim", postfix: "verbatim"},
       {type: "categories", postfix: "categories"},
@@ -159,30 +165,30 @@ class Hitlist {
       {type: "sentiment", postfix: "sentiment"}
     ];
 
-    types.forEach( type => {
+    types.forEach(type => {
       if (this.headers[type.type] && this.headers[type.type].length > 0)
-        this.headers[type.type].forEach( question =>{
-          this.addClassesToHitlist(question,type);
+        this.headers[type.type].forEach(question => {
+          this.addClassesToHitlist(question, type);
           this.changeTitles(question);
         })
     })
   }
 
-  movePaginationToTheMainHeader(){
+  movePaginationToTheMainHeader() {
     var title = this.source.querySelector(".yui3-datatable-header.reportal-hitlist-main").innerText;
-    this.source.querySelector(".yui3-datatable-header.reportal-hitlist-main").innerHTML="";
-    var paginationText =title+" "+this.source.getElementsByClassName("hitlist-pagination-count")[0].innerText.replace("(","of ").slice(0,-1);
+    this.source.querySelector(".yui3-datatable-header.reportal-hitlist-main").innerHTML = "";
+    var paginationText = title + " " + this.source.getElementsByClassName("hitlist-pagination-count")[0].innerText.replace("(", "of ").slice(0, -1);
     var paginationElement = document.createElement("span");
     paginationElement.innerText = paginationText;
     this.source.querySelector(".yui3-datatable-header.reportal-hitlist-main").appendChild(paginationElement);
   }
 
-  processSortableColumns(){
+  processSortableColumns() {
     var sortableColumns = this.source.querySelectorAll(".yui3-datatable-header.yui3-datatable-sortable-column");
-    [].slice.call(sortableColumns).forEach( header => this.addSorting( header ));
+    [].slice.call(sortableColumns).forEach(header => this.addSorting(header));
   }
 
-  addSorting(header){
+  addSorting(header) {
     var dateSortingElement = document.createElement("span");
     dateSortingElement.innerText = header.innerText;
     dateSortingElement.classList.add("sortable");
@@ -190,73 +196,73 @@ class Hitlist {
     this.toggleSorting(dateSortingElement, header);
     header.innerHTML = "";
     header.appendChild(dateSortingElement);
-    dateSortingElement.addEventListener("click",(event)=>{
+    dateSortingElement.addEventListener("click", (event) => {
       dateSortingElement.classList.add("waiting");
       dateSortingElement.classList.remove("sorted");
     });
   }
 
-  toggleSorting(targetElement,sourceElement = targetElement){
-    if(sourceElement.classList.contains("yui3-datatable-sortable-column") && sourceElement.classList.contains("yui3-datatable-sorted")){
+  toggleSorting(targetElement, sourceElement = targetElement) {
+    if (sourceElement.classList.contains("yui3-datatable-sortable-column") && sourceElement.classList.contains("yui3-datatable-sorted")) {
       targetElement.classList.add("sorted");
-      if( sourceElement.getAttribute("aria-sort") == "ascending" ){
+      if (sourceElement.getAttribute("aria-sort") == "ascending") {
         targetElement.classList.add("asc");
         targetElement.classList.remove("desc");
-      }else{
+      } else {
         targetElement.classList.add("desc");
         targetElement.classList.remove("asc");
       }
     }
   }
 
-  addIconsForSentiment(){
+  addIconsForSentiment() {
     let sentimentCells = this.source.querySelectorAll(".yui3-datatable-cell.reportal-hitlist-sentiment");
-    if(sentimentCells && sentimentCells.length > 0){
-      [].slice.call(sentimentCells).forEach(cell=>this.addIconForSentiment(cell));
+    if (sentimentCells && sentimentCells.length > 0) {
+      [].slice.call(sentimentCells).forEach(cell => this.addIconForSentiment(cell));
     }
 
   }
 
-  addIconForSentiment(cell){
+  addIconForSentiment(cell) {
     var value = parseInt(cell.innerText);
-    for(var i = 0; i < this.sentimentConfig.length; i++){
-      if(value <= this.sentimentConfig[i].range.max && value >= this.sentimentConfig[i].range.min){
+    for (var i = 0; i < this.sentimentConfig.length; i++) {
+      if (value <= this.sentimentConfig[i].range.max && value >= this.sentimentConfig[i].range.min) {
         cell.innerHTML = this.sentimentConfig[i].icon ? this.sentimentConfig[i].icon : this.icons[this.sentimentConfig[i].sentiment];
       }
     }
   }
 
-  processMainColumn(){
+  processMainColumn() {
     var mainCells = this.source.querySelectorAll(".yui3-datatable-cell.reportal-hitlist-main");
-    [].slice.call(mainCells).forEach((cell, index)=>{
+    [].slice.call(mainCells).forEach((cell, index) => {
       this.wrapComment(cell);
 
-      if(this.selectedWords) {
-      //if(false) {
+      if (this.selectedWords) {
+        //if(false) {
         this.highlightSelectedWords(cell, index);
       }
 
       //this.addDateToComment(cell, index);
-      if(this.headers["categories"]) {
+      if (this.headers["categories"]) {
         this.addCategoriesToComment(cell, index);
       }
     });
   }
 
-  processDates(){
+  processDates() {
     var dateCells = this.source.querySelectorAll(".yui3-datatable-cell.reportal-hitlist-date");
-    [].slice.call(dateCells).forEach((cell, index)=>{
+    [].slice.call(dateCells).forEach((cell, index) => {
       var dateTimeFormat = this.dateTimeFormat;
       var date = dateTimeFormat ? dateformat(new Date(cell.innerText), dateTimeFormat) : cell.innerText;
       cell.innerHTML = "";
       var dateElement = document.createElement("div");
       dateElement.innerText = date;
       dateElement.classList.add("hitlist-date-info");
-      cell.insertBefore(dateElement,cell.firstChild);
+      cell.insertBefore(dateElement, cell.firstChild);
     });
   }
 
-  wrapComment(cell){
+  wrapComment(cell) {
     var comment = document.createElement("div");
     comment.innerText = cell.innerText;
     cell.innerHTML = "";
@@ -279,11 +285,11 @@ class Hitlist {
       .filter(category =>
         currentCategory ?
           (separator ?
-            category.startsWith(currentCategory) :
-            categories.every(item =>
-              (item === currentCategory && category.startsWith(item)) ||
-              (item !== currentCategory && (item.length <= currentCategory.length || item.length > currentCategory.length && !category.startsWith(item)))
-            )
+              category.startsWith(currentCategory) :
+              categories.every(item =>
+                (item === currentCategory && category.startsWith(item)) ||
+                (item !== currentCategory && (item.length <= currentCategory.length || item.length > currentCategory.length && !category.startsWith(item)))
+              )
           ) :
           true
       )
@@ -346,7 +352,7 @@ class Hitlist {
     }
   }
 
-  createCategoryCard(category){
+  createCategoryCard(category) {
     const categoryCard = document.createElement("span");
     categoryCard.innerText = category;
     categoryCard.classList.add("hitlist-tag");
@@ -366,7 +372,7 @@ class Hitlist {
     mainCategoryCard.innerText = mainCategoty;
 
     const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-    if(isFirefox) {
+    if (isFirefox) {
       mainCategoryCard.classList.add('firefox');
     }
 
@@ -407,6 +413,48 @@ class Hitlist {
     document.body.removeChild(newElement);
 
     return width;
+  }
+
+  addInfoText() {
+    const container = this.source;
+
+    let infoTextClass = 'ta-info-text';
+    let infoText = this.source.querySelector('.' + infoTextClass);
+
+    if (!infoText) {
+      infoText = document.createElement('div');
+      infoText.className = infoTextClass;
+      infoText.style.display = 'none';
+      infoText.innerText = 'All comments are shown here and you can select to filter on the theme, ' +
+        'sub category and attribute to view as well as the sentiment for that level of expression. ' +
+        'Sentiment is shown at both the attribute level and at the overall comment level.\n\n' +
+        'If you want to sort the comments page click on any column header';
+    }
+
+    let infoIconClass = 'ta-info-icon';
+    let infoIcon = this.source.querySelector('.' + infoIconClass);
+
+    if (!infoIcon) {
+      infoIcon = document.createElement('div');
+      infoIcon.className = infoIconClass;
+      infoIcon.style.float = 'right';
+
+      infoIcon.onmouseover = () => {
+        infoText.style.display = '';
+        const rect = infoIcon.getBoundingClientRect();
+        infoText.style.top = (rect.top + 32 + window.pageYOffset) + 'px';
+        infoText.style.right = (window.innerWidth - rect.right - 32) + 'px';
+        infoText.style.width = (container.clientWidth * 0.8) + 'px';
+      };
+
+      infoIcon.onmouseout = () => {
+        infoText.style.display = 'none';
+      };
+    }
+
+    const filterContainer = container.querySelector('.hitlist-active-filters.reportal-clearfix');
+    filterContainer.parentNode.insertBefore(infoIcon, filterContainer);
+    container.appendChild(infoText);
   }
 }
 
