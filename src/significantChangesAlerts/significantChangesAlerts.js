0908817570af10459a14400d913b1ea8bf3b6b43
Anna Kozlova <fyz95@mail.ru>
2018-09-18 15:15:11 +0300

    Sig Change Alerts in studio style merged with usual alerts

--- src/significantChangesAlerts/significantChangesAlerts.js
+++ src/significantChangesAlerts/significantChangesAlerts.js
@@ -1,9 +1,11 @@
 export default class SignificantChangesAlerts {
 
-  constructor({tableId, containerId, separator}) {
+  constructor({tableId, containerId, translations, separator, period}) {
     this.table = document.getElementById(tableId);
     this.container = document.getElementById(containerId);
-    this.separator = (!!separator && separator != "") ? separator : "|";
+    this.translations = translations;
+    this.period = period;
+    this.separator = (!!separator && separator !== "") ? separator : "|";
     this.alerts = [];
     this.init();
   }
@@ -11,80 +13,110 @@ export default class SignificantChangesAlerts {
   init() {
     this.getDataFromTable();
     this.createCards();
-    this.setCardsHeight();
   }
 
   getDataFromTable() {
     let markedCells;
-    markedCells  = [...this.table.querySelectorAll('body td.increasingC:last-child, tbody td.decreasingC:last-child, tbody td.increasingS:last-child, tbody td.decreasingS:last-child')];
+    markedCells = [...this.table.querySelectorAll('body td.increasingC:last-child, tbody td.decreasingC:last-child, tbody td.increasingS:last-child, tbody td.decreasingS:last-child')];
     markedCells.forEach(cell => {
       let categoryText = "";
       let changesText = "";
       let drilldownRef = cell.parentElement.firstElementChild.firstElementChild;
       cell.parentElement.firstElementChild.innerText.split(this.separator).forEach(categoryLevel => {
-        categoryText += categoryLevel.trim() + '\n';
+        categoryText += categoryLevel.trim() + '<br>';
       });
-      const changeTypesArray = [{class:'increasingC', text:'<span class="alert-card__increasing">increased</span> in Volume'},
-                                {class:'increasingS', text:'<span class="alert-card__increasing">increased</span> in Sentiment'},
-                                {class:'decreasingC', text:'<span class="alert-card__decreasing">decreased</span> in Volume'},
-                                {class:'decreasingS', text:'<span class="alert-card__decreasing">decreased</span> in Sentiment'}];
+      const changeTypesArray = [{
+        class: 'increasingC',
+        text: 'increased in Volume',
+        r2class: 'target__number r2i-green-color'
+      },
+        {class: 'increasingS', text: 'increased in Sentiment', r2class: 'target__number r2i-green-color'},
+        {class: 'decreasingC', text: 'decreased in Volume', r2class: 'target__number r2i-dark-red-color'},
+        {class: 'decreasingS', text: 'decreased in Sentiment', r2class: 'target__number r2i-dark-red-color'}];
       changeTypesArray.forEach(classItem => {
-        if (cell.classList.contains(classItem.class))
-          changesText += classItem.text+ '<br>';
+        if (cell.classList.contains(classItem.class)) {
+          changesText += classItem.text + '\n';
+          categoryText = "<span class = '" + classItem.r2class + "'>" + categoryText + "</span>";
+        }
       });
       this.alerts.push({categoryText: categoryText, changesText: changesText, drilldownRef: drilldownRef});
     })
   }
 
   createCards() {
-    this.alerts.forEach(alertItem => {
-      let alertCard = document.createElement('div');
-      alertCard.className = 'alert-card';
+    let rowContainer;
+    rowContainer = document.createElement('div');
+    rowContainer.className = 'r2i-row r2i-row--max-width';
+    for (let i = 0; i < this.alerts.length; i++)
+      rowContainer.appendChild(this.createCard(i));
 
-      let alertHeader = document.createElement('div');
-      alertHeader.innerText = 'Category';
-      alertHeader.className = 'alert-card__title';
+    if (this.alerts.length > 0) {
+      let headerContainer = document.createElement('div');
+      headerContainer.className = "r2i-row r2i-row--max-width";
+      let headerTitleWidget = document.createElement('div');
+      headerTitleWidget.className = "r2-title-widget";
+      let headerTitleView = document.createElement('div');
+      headerTitleView.className = "r2-title-view";
+      let headerTitleViewName = document.createElement('div');
+      headerTitleViewName.className = "r2-title-view__name";
 
-      let alertCategory  = document.createElement('div');
-      alertCategory.innerText = alertItem.categoryText;
-      alertCategory.className = 'alert-card__category';
+      headerTitleViewName.innerText = this.translations['What has significantly changed last '] + this.translations[this.period];
 
+      this.container.appendChild(headerContainer);
+      headerContainer.appendChild(headerTitleWidget);
+      headerTitleWidget.appendChild(headerTitleView);
+      headerTitleView.appendChild(headerTitleViewName);
+    }
 
-      let alertChanges = document.createElement('div');
-      alertChanges.innerHTML = alertItem.changesText;
-      alertChanges.className = 'alert-card__changes';
-
-
-      alertCard.appendChild(alertHeader);
-      alertCard.appendChild(alertCategory);
-      alertCard.appendChild(alertChanges);
-      alertCard.onclick = () => {
-       alertItem.drilldownRef.click();
-      }
-
-      this.container.appendChild(alertCard);
-      this.container.className += " alerts-content";
-    });
+    this.container.appendChild(rowContainer);
   }
 
-  setCardsHeight() {
-     const categoriesSections = [...this.container.querySelectorAll('.alert-card__category')];
-     const maxHeightCategories = categoriesSections.length > 0 ? categoriesSections.reduce((res, cur) => cur.clientHeight > res.clientHeight ? cur : res).clientHeight : 0;
-     categoriesSections.forEach(title => title.style.height = maxHeightCategories + 'px');
+  createCard(index) {
+    let alertItem = this.alerts[index];
+    let alertCard = document.createElement('article');
+    alertCard.className = 'dashboard__widget dashboard__widget--small r2i-widget r2i-TA-x-small';
 
-     const changesSections = [...this.container.querySelectorAll('.alert-card__changes')];
-     const maxHeightChanges = changesSections.length > 0? changesSections.reduce((res, cur) => cur.clientHeight > res.clientHeight ? cur : res).clientHeight : 0;
-     changesSections.forEach(title => title.style.height = maxHeightChanges + 'px');
+    let alertHeader = document.createElement('header');
+    alertHeader.className = 'widget__header';
+    let alertTitle = document.createElement('h3');
+    alertTitle.className = 'widget__title';
+    alertTitle.innerText = 'Category';
+    alertHeader.appendChild(alertTitle);
+
+
+    let alertCategory = document.createElement('div');
+    alertCategory.innerHTML = alertItem.categoryText;
+
+    let alertChanges = document.createElement('div');
+    alertChanges.innerText = alertItem.changesText;
+    alertChanges.className = 'small__text';
+
+    let widgetBody = document.createElement('div');
+    widgetBody.className = 'widget__body widget__body--no-scrolling';
+    let kpiElement = document.createElement('div');
+    kpiElement.className = 'co-st-r-widget-kpi r2i-kpi-content-center';
+    let mainContent = document.createElement('div');
+
+    widgetBody.appendChild(kpiElement);
+    kpiElement.appendChild(mainContent);
+    mainContent.appendChild(alertCategory);
+    mainContent.appendChild(alertChanges);
+
+    let buttonsGroup = document.createElement('div');
+    buttonsGroup.className = 'r2i-buttons-group-right';
+    let button = document.createElement('button');
+    button.className = "comd-button___studio";
+    button.innerHTML = this.translations['Alert Button'];
+    buttonsGroup.appendChild(button);
+
+    alertCard.appendChild(alertHeader);
+    alertCard.appendChild(widgetBody);
+    alertCard.appendChild(buttonsGroup);
+
+    button.onclick = () => {
+      alertItem.drilldownRef.click();
+    };
+
+    return alertCard;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
